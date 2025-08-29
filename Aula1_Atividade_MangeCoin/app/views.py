fc319281b4b6181736858e61cdf38ca1abc5f6a6
LucasSantino <lsantino541@gmail.com>
2025-08-28 23:03:57 -0300

    up

--- Aula1_Atividade_MangeCoin/app/views.py
+++ Aula1_Atividade_MangeCoin/app/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+#responavel pelo CRUD da API
+from rest_framework.viewsets import ModelViewSet
 
-# Create your views here.
+from .serializers import *
+from .models import *
+
+class CustomUserView(ModelViewSet):
+    queryset = CustomUser.objects.all()
+    serializer_class = CustomUserSerializer
+
+class AccountView(ModelViewSet):
+    queryset = Account.objects.all()
+    serializer_class = AccountSerializer
+
+class TokenView(ModelViewSet):
+    queryset = Token.objects.all()
+    serializer_class = TokenSerializer
+
+class AccountTokenView(ModelViewSet):
+    queryset = AccountToken.objects.all()
+    serializer_class = AccountTokenSerializer
+
+class TransactionView(ModelViewSet):
+    queryset = Transaction.objects.all()
+    serializer_class = TransactionSerializer
+
+class BetView(ModelViewSet):
+    queryset = Bet.objects.all()
+    serializer_class = BetSerializer
